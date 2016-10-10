8d0684d75bccaf1eca51639d376849e920690ace
Marcelo Bartsch <ruhlem@amazon.com>
2016-10-10 16:35:44 +0100

    start adding the ddns support

--- Scripts/docker-ddns.py
+++ Scripts/docker-ddns.py
@@ -1,72 +1,93 @@
+#!/usr/bin/env python3
 import docker
-import docker_events
 import json
 import sys
+import dns
 
 
+tsigfile = 'secrets.json'
 client = docker.Client()
+tsighandle = open(tsigfile, mode='r')
+keyring = dns.tsigkeyring.from_text(json.load(tsighandle))
+tsighandle.close()
+
 
 def startup():
-  containers = []
+    containers = []
 
-  for container in client.containers():
-    container_id = container["Id"]
-    inspect=client.inspect_container(container_id)
-    container_hostname = inspect["Config"]["Hostname"]
-    container_name = inspect["Name"].split('/', 1)[1]
-    networkmode=str(inspect["HostConfig"]["NetworkMode"])
-    if ((str(networkmode) != 'host') and 'container:' not in networkmode): # and (str(networkmode) != 'default')):
-      if (str(networkmode) != 'default'):
-        container_ip = container["NetworkSettings"]["Networks"][networkmode]["IPAddress"]
-      else:
-        container_ip = container["NetworkSettings"]["Networks"]["bridge"]["IPAddress"]
-      #print("Updating %s to ip (%s|%s|%s) -> %s" % (container_id, container_hostname, container_name, networkmode, container_ip))
-    event_data = { container['Id']: {
-           "status": "running",
-           "Hostname": container_hostname,
-           "from": container['Image'],
-           "time": container['Created'],
-           "ipAddr": container_ip }}
-         
-    containers.append(event_data)
-  return json.dumps(containers)
+    for container in client.containers():
+        container_id = container["Id"]
+        inspect = client.inspect_container(container_id)
+        container_hostname = inspect["Config"]["Hostname"]
+        container_name = inspect["Name"].split('/', 1)[1]
+        networkmode = str(inspect["HostConfig"]["NetworkMode"])
+        # and (str(networkmode) != 'default')):
+        if ((str(networkmode) != 'host') and 'container:' not in networkmode):
+            if (str(networkmode) != 'default'):
+                container_ip = container["NetworkSettings"][
+                    "Networks"][networkmode]["IPAddress"]
+            else:
+                container_ip = container["NetworkSettings"][
+                    "Networks"]["bridge"]["IPAddress"]
+        event_data = {container['Id']: {
+            "status": "running",
+            "name": container_name,
+            "Hostname": container_hostname,
+            "from": container['Image'],
+            "time": container['Created'],
+            "ipAddr": container_ip}}
 
-def container_info(containerId): 
-    container={}
+        containers.append(event_data)
+    return json.dumps(containers)
+
+
+def container_info(containerId):
+    container = {}
     print(containerId)
-    inspect=client.inspect_container(containerId)
-    networkmode=str(inspect["HostConfig"]["NetworkMode"])
+    inspect = client.inspect_container(containerId)
+    networkmode = str(inspect["HostConfig"]["NetworkMode"])
     container['name'] = inspect["Name"].split('/', 1)[1]
-    if ((str(networkmode) != 'host') and 'container:' not in networkmode): 
-        if (str(networkmode) != 'default'): 
-            container['ip'] = inspect["NetworkSettings"]["Networks"][networkmode]["IPAddress"] 
-        else: 
-            container['ip'] = inspect["NetworkSettings"]["Networks"]["bridge"]["IPAddress"]
+    if ((str(networkmode) != 'host') and 'container:' not in networkmode):
+        if (str(networkmode) != 'default'):
+            container['ip'] = inspect["NetworkSettings"][
+                "Networks"][networkmode]["IPAddress"]
+        else:
+            container['ip'] = inspect["NetworkSettings"][
+                "Networks"]["bridge"]["IPAddress"]
     else:
-        container['ip']='0.0.0.0'
+        container['ip'] = '0.0.0.0'
     container['hostname'] = inspect["Config"]["Hostname"]
     return container
 
 
-	
+def dockerddns(action, event, dnsserver, ttl):
+    update = dns.update.Update('i.bartsch.cl.', keyring=keyring)
+    update.replace(event['hostname'], ttl, 'A', event['ip'])
+    response = dns.query.tcp(update, dnsserver, timeout=10)
+    print(response)
+    print("hello")
+
+
 def process():
-    containerinfo={}
+    containerinfo = {}
     events = client.events(decode=True)
     for event in events:
         if event['Type'] == "container":
             if event['Action'] == 'start':
-                containerinfo=container_info(event['id'])
+                containerinfo = container_info(event['id'])
                 print(containerinfo)
-                print("Container %s is starting with hostname %s and ipAddr %s" % (containerinfo['name'],
-                    containerinfo['hostname'],containerinfo['ip']))
+                print("Container %s is starting with hostname %s and ipAddr %s"
+                      % (containerinfo['name'],
+                         containerinfo['hostname'], containerinfo['ip']))
             elif event['Action'] == 'die':
-                containerinfo=container_info(event['id'])
+                containerinfo = container_info(event['id'])
                 print(containerinfo)
-                print("Container %s is stopped %s" % (containerinfo['name'],
-                    containerinfo['hostname']))
+                print("Container %s is stopping %s" %
+                      (containerinfo['name'],
+                       containerinfo['hostname']))
 
 print(startup())
-try: 
+try:
     process()
 except KeyboardInterrupt:
     print("Bye")
